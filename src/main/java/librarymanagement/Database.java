07c392b84400f8c32310b8a2360dd1375ee6a1db
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-07-20 15:11:23 +0500

    complete the program

--- src/main/java/librarymanagement/Database.java
+++ src/main/java/librarymanagement/Database.java
@@ -1,12 +1,13 @@
 package librarymanagement;
 
 import java.io.*;
+
 public class Database {
     public static void saveLibrary(Library library, String filename) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
             oos.writeObject(library);
-        } catch (IOException e ) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error saving data: " + e.getMessage());
         }
     }
 
@@ -14,9 +15,8 @@ public class Database {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
             return (Library) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+            System.err.println("No existing data found, starting with an empty database.");
             return new Library();
         }
     }
 }
-
